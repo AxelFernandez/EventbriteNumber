0faa5a453742fa0fb0d7d53466752d7148525353
Axel <axel_fernandez18@hotmail.com>
2019-05-15 22:18:09 -0300

    Commit 15/05

--- Test/com/axelfernandez/AbstractStrategyTest.java
+++ Test/com/axelfernandez/AbstractStrategyTest.java
@@ -2,21 +2,33 @@ package com.axelfernandez;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractStrategyTest {
+    AbstractStrategy abstractStrategy = new HumanPcStrategy();
 
     @Test
     void validateNumber() {
-    AbstractStrategy abstractStrategy = new HumanPcStrategy();
-    boolean result =  abstractStrategy.validateNumber("2312");
+    boolean result =  abstractStrategy.validateNumber("2314");
     assertTrue(result);
 
     result = abstractStrategy.validateNumber("1111");
+    assertFalse(result);
+
+    result = abstractStrategy.validateNumber("number");
+    assertFalse(result);
+
+    result = abstractStrategy.validateNumber("12345");
     assertFalse(result);
     }
 
     @Test
     void analyzeAttempt() {
+        List result;
+       int number = abstractStrategy.secretNumber.getSecretNumber();
+        result = abstractStrategy.analyzeAttempt(number);
+        //assertArrayEquals(String [0,4] ,[0,4]);
     }
 }
